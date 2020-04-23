e86330728f287243b627ad534bea65d47fc10c80
sohye <sohye@squarelab.co>
2020-04-24 02:39:16 +0900

    refactor: Header and Layout UI

--- src/views/layout/Header.tsx
+++ src/views/layout/Header.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
+export const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  box-shadow: rgb(238, 238, 239) 0px -1px 0px inset;
+  padding: 0;
+  margin: 0;
+`;
+
 export const HeaderContainer = styled.div`
-      width: 100%;
-      height: 50px;
-      max-width: 1220px;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-      padding: 14px 0;
+  width: 1170px;
+  max-width: 1170px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 14px 0;
 `;
 
 const Header: React.FC = () => (
-  <HeaderContainer>
-    <img src='https://class101.net/images/class101-main-logo.svg' alt='class101 main logo' />
-    <span>장바구니</span>
-  </HeaderContainer>
+  <HeaderWrapper>
+    <HeaderContainer>
+      <img src='https://class101.net/images/class101-main-logo.svg' alt='class101 main logo' />
+      <span>장바구니</span>
+    </HeaderContainer>
+  </HeaderWrapper>
 );
 
 export default Header;
--- src/views/layout/Layout.tsx
+++ src/views/layout/Layout.tsx
@@ -3,19 +3,21 @@ import styled from 'styled-components';
 import Header from './Header';
 
 export const ContentLayout = styled.div`
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      height: 100%;
-      max-width: 1220px;
-      margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  max-width: 1170px;
+  margin: 0 auto;
 `;
 
 const Layout: React.FC = ({ children }) => (
-  <ContentLayout>
+  <>
     <Header />
-    {children}
-  </ContentLayout>
+    <ContentLayout>
+      {children}
+    </ContentLayout>
+  </>
 );
 
 export default Layout;
